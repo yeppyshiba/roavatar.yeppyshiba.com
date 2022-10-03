ff7914ec74a16df335ceb04c03c5d0584dd7b093
monoless <ecst@monolife.co.kr>
2022-10-03 17:46:52 +0900

    change to next

--- src/enums/Path.ts
+++
@@ -1,6 +0,0 @@
-enum Path {
-    HOME = "/",
-    GENERATOR = "/add",
-}
-
-export default Path;
--- src/pages/Generator.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
-
-const Generator = () => {
-    return <Text>Generator</Text>;
-};
-
-export default Generator;
